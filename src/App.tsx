6fc0521c2199c0e338b9a49f219d99f2ea460ee2
MH <mudasir.ee@hotmail.com>
2023-08-17 10:17:28 +0500

    Test CI changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        Test changes
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
